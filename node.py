66e880aa2195bc4fbbb58ef7d744bcce8a73108c
Jason Benn <jasoncbenn@gmail.com>
2016-03-06 16:23:49 -0800

    Nodes can boot up, connect to any other node and ask the node they're connecting to for keyspace.

--- node.py
+++ node.py
@@ -1,58 +1,120 @@
-from sys import argv
-import socket
+import gevent
+from gevent import socket
+import sys
+
+
+class Keyspace(object):
+    def __init__(self, lower, upper):
+        self.lower = float(lower)
+        self.upper = float(upper)
+
+    def __str__(self):
+        return "(%s, %s)" % (self.lower, self.upper)
+
+    def subdivide(self):
+        upper = self.upper
+        midpoint = ((self.upper - self.lower) / 2) + self.lower
+        self.upper = midpoint
+        return Keyspace(midpoint, upper)
+
+    def serialize(self):
+        return "%s-%s" % (self.lower, self.upper)
+
+    @classmethod
+    def unserialize(self, keyspace):
+        return Keyspace(*keyspace.split("-"))
 
 
 class Node(object):
-    def __init__(self, port):
-        self.port = port
-        pass
-        # self.keyspace = keyspace
+    def __init__(self, own_port=None, keyspace=None):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+        self.socket.bind(("localhost", own_port or 0))  # 0 Chooses random port
+        self.port = self.socket.getsockname()[1]
+        self.keyspace = keyspace
 
-    def subdivide(self):
-        # return the other half as a keyspace tuple (to be used by requester)
-        # reduce own keyspace by half on success
-        pass
-
-    def query(self, query):
-        return "Queried:", query
+    def __str__(self):
+        return "node:%s" % self.port
 
     def join_network(self, entry_port):
-        # send request (tcp? udp?) to passed port - that node will subdivide
-        # then i'll receive a keyspace
-        # set keyspace on self, return
-        print "localhost:%s" % entry_port
-        return (0, 0.5)
-        # sendto API
+        print "%s: JOINing %s" % (self, entry_port)
+        self.socket.sendto("JOIN", ("localhost", entry_port))
+
+    def query(self, request):
+        query = request[0]
+        sender = request[1]
+        response = None
+
+        print "Received \"%s\" from %s." % (query, sender)
+
+        if query == "JOIN":
+            response = "SETKEYSPACE " + self.subdivide().serialize()
+            print "Own keyspace is now %s" % self.keyspace
+
+        elif query.startswith("SETKEYSPACE "):
+            self.keyspace = Keyspace.unserialize(query.split(" ")[1])
+            print "Received keyspace: %s." % self.keyspace
+
+        elif query.startswith("GET"):
+            arg = query.split()[1]
+            response = "GET: %s." % arg
+
+        elif query.startswith("PUT"):
+            _, key, value = query.split()
+            response = "PUT: %s: %s." % (key, value)
+
+        else:
+            print "Unrecognized query \"%s\"." % query
+
+        if response:
+            self.socket.sendto(response, sender)
+
+    def subdivide(self):
+        return self.keyspace.subdivide()
 
 
-if __name__ == '__main__':
-    # Ask for a own_port
-    # TODO: just directly ask for a port
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("", 0))
-    own_port = s.getsockname()[1]
+def start_first_node():
+    node = Node(own_port=60000, keyspace=Keyspace(0, 1))
+    print "Started new DHT with %s" % node
+    return node
 
-    # Init node with own_port
-    node = Node(own_port)
 
-    # Get Keyspace
-    try:
-        # Assume I'm passed a port...
-        # try to connect to the passed port, ask to share its space
-        entry_port = argv[1]
-        keyspace = node.join_network(entry_port)  # ask the port for a keyspace tuple
-        node.keyspace = keyspace
-    except IndexError:
-        node.keyspace = (0, 1)
+def start_node(entry_port):
+    node = Node()
+    node.join_network(entry_port)
+    print "Started %s, now joining." % node
+    return node
 
-    print node.keyspace
-    # Listen to this own_port
-    s.listen(1)
 
-    # Wait for input
-    while True:
-        query = raw_input("Query: ")
-        response = node.query(query)[1]
-        print response
-        # get 1
-        # put 5 1
+try:
+    entry_port = int(sys.argv[1])
+    node = start_node(entry_port)
+except IndexError:
+    node = start_first_node()
+
+
+def await_query(node):
+    socket.wait_read(sys.stdin.fileno())
+    data = sys.stdin.readline().rstrip("\n")
+    return data
+
+
+def await_request(node):
+    print 'Listening on port %s.' % node.port
+    return node.socket.recvfrom(1024)  # Buffer size is 1024 bytes
+
+
+query = gevent.spawn(await_query, node)
+request = gevent.spawn(await_request, node)
+
+while True:
+    if query.successful():
+        print "Sending user input!"
+        node.socket.sendto(query.value, ("localhost", entry_port))
+        query = gevent.spawn(await_query, node)
+        gevent.sleep(0)
+    if request.successful():
+        print "Greenlet exited successfully with", request.value
+        node.query(request.value)
+        request = gevent.spawn(await_request, node)
+        gevent.sleep(0)
+    gevent.sleep(0)
