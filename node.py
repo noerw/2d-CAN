0e8d57adda2d856b74825b2e1ba1016bd5ad89f9
Norwin Roosen <github@nroo.de>
2019-07-31 10:19:18 +0200

    WIP add GridTopology
    used to track neighbours and route to the right ones

--- node.py
+++ node.py
@@ -1,46 +1,101 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import division
 from gevent import socket
 from functools import partial
 from keyspace import Keyspace
 from hashlib import md5
 import json
-# import commands
 
+# class Direction(object):
+#     NORTH = 'n'
+#     WEST  = 'w'
+#     SOUTH = 's'
+#     EAST  = 'i'
+#     LOCAL = 'l'
+#     DIRECTIONS = [NORTH, WEST, SOUTH, EAST]
+#     items = { d: None for d in DIRECTIONS }
+#     def __init(self, items=None):
+#         if items:
+#             self.items = items
+
+class Direction(object):
+    north = 'n'
+    west  = 'w'
+    south = 's'
+    east  = 'i'
+    local = 'l'
+
+# class GridTopology(Direction):
 class GridTopology(object):
     # 2D topology, keeping track of neighbours and their keyspaces
 
-    # ownKeyspace = Keyspace()
-    left   = []
-    right  = []
-    top    = []
-    bottom = []
+    keyspace = None
+    north = []
+    west  = []
+    south = []
+    east  = []
 
-    def addTo(self, direction, address, keyspace):
-        if direction == 'left':
-            self.left.append((
-                adress,
-                keyspace
-            ))
-        else:
-            raise Exception('not implemented')
+    def __init__(self, keyspace):
+        self.keyspace = keyspace
+
+    def addNeighbour(self, direction, address, keyspace):
+        if   direction == Direction.north: self.north.append((address, keyspace))
+        elif direction == Direction.west:  self.west.append((address, keyspace))
+        elif direction == Direction.south: self.south.append((address, keyspace))
+        elif direction == Direction.east:  self.east.append((address, keyspace))
+        else: raise Exception('invalid direction "%s"' % direction)
 
     def getDirection(self, point):
         # compare with self.keyspace
+        # if we're out of bounds both on x and y, we move on x first
+        # TODO: improve this logic by preferring larger nodes first,
+        # as they know more neighbours, reducing hops
+        minx, miny = self.keyspace.lower
+        maxx, maxy = self.keyspace.upper
+        x, y = point
+
+        if x < minx:   return Direction.west
+        elif x > maxx: return Direction.east
+        elif y < miny: return Direction.north
+        elif y > maxy: return Direction.south
+        else:          return Direction.local # TODO: should we handle this case like that?
 
     def getNeighboursForDirection(self, direction):
-        if direction == 'left': return self.left
-        else:                   raise Exception('not implemented')
+        if   direction == Direction.north: return self.north
+        elif direction == Direction.west:  return self.west
+        elif direction == Direction.south: return self.south
+        elif direction == Direction.east:  return self.east
+        else: raise Exception('invalid direction "%s"' % direction)
 
     def getNeighbourForPoint(self, point):
+        # find best neighbour in respective direction list
         # TODO: search for optimal neighbour (-> routing basically)
         x, y = point
 
         direction = self.getDirection(point)
+        if direction == Direction.local:
+            raise Exception('not implemented')
+
         neighbours = self.getNeighboursForDirection(direction)
+        # TODO: handle case were we have no neighbours in a direction?
 
-        # TODO: find best neighbour in respective directoin list
-        raise Exception('not implemented')
+        bestNeigbour = None
+        minDiff = 999999999999
+        for address, keyspace in neighbours:
+            minx, miny = keyspace.lower
+            maxx, maxy = keyspace.upper
+            # finding the optimal neighbour on the other axis
+            if direction in [Direction.west, Diretion.east]:
+                diff = abs(y < miny)
+            elif direction in [Direction.north, Direction.south]:
+                diff = abs(x < minx)
 
+            if diff < minDiff:
+                minDiff = diff
+                bestNeighbour = (address, keyspace)
+
+        return bestNeighbour
 
 
 class Node(object):
@@ -116,6 +171,8 @@ class Node(object):
 
         if query == "JOIN":
             # TODO: join does not forward to other nodes (needs to provide a (random) point)
+            # or should we allow a stateless GET, so the joining node finds the respective node prior to joining?
+
             # if point within own keyspace:
                 # subdivide keyspace
                 # send half of keyspace to new node
@@ -187,3 +244,12 @@ class Node(object):
 
         else:
             print ("Unrecognized query \"%s\"." % query)
+
+'''
+# z-order <-> binary tree relationship
+- until 4 bits: one neighbour in each branch, going one level up each
+-
+
+# z-order <-> de-briujn
+
+'''
